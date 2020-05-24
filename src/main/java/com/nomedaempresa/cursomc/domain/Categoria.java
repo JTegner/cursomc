d6da52df4f962acca0a5f3150bf1ea0dda19f3bf
Jordano Tegner <jordano.tegner@gmail.com>
2020-05-24 14:40:48 -0300

    banco de dados postgres e criacao automatica da base de dados

--- src/main/java/com/nomedaempresa/cursomc/domain/Categoria.java
+++ src/main/java/com/nomedaempresa/cursomc/domain/Categoria.java
@@ -2,8 +2,14 @@ package com.nomedaempresa.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
+@Entity
 @Data
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -13,6 +19,8 @@ public class Categoria implements Serializable {
 	 * rede. Eh uma exigencia da linguagem java
 	 */
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
