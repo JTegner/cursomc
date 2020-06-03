eb065714b64bac2e00c6cd4937965747e4b15f33
Jordano Tegner <jordano.tegner@gmail.com>
2020-06-02 22:48:29 -0300

    validação com bean validation

--- src/main/java/com/nomedaempresa/cursomc/services/CategoriaService.java
+++ src/main/java/com/nomedaempresa/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.nomedaempresa.cursomc.domain.Categoria;
+import com.nomedaempresa.cursomc.dto.CategoriaDTO;
 import com.nomedaempresa.cursomc.repositories.CategoriaRepository;
 import com.nomedaempresa.cursomc.services.exception.DataIntegrityException;
 import com.nomedaempresa.cursomc.services.exception.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
     public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
     	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
     	return categoriaRepository.findAll(pageRequest);
+    }
+    
+    public Categoria fromDTO(CategoriaDTO objDto) {
+    	return new Categoria(objDto.getId(), objDto.getNome());
     }
 	
 }
