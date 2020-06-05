00c7e2c49145d5c63d42251e3712d975ec96c9d1
Jordano Tegner <jordano.tegner@gmail.com>
2020-06-05 20:07:07 -0300

    Correcoes

--- src/main/java/com/nomedaempresa/cursomc/services/CategoriaService.java
+++ src/main/java/com/nomedaempresa/cursomc/services/CategoriaService.java
@@ -32,10 +32,11 @@ public class CategoriaService {
 		obj.setId(null);
 		return categoriaRepository.save(obj);
 	}
-	
+		
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return categoriaRepository.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return categoriaRepository.save(newObj);
 	}
 	
 	public void delete(Integer id) {
@@ -44,7 +45,7 @@ public class CategoriaService {
 			categoriaRepository.deleteById(id);
 		} 
 		catch (DataIntegrityViolationException e) {
-			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			throw new DataIntegrityException("Não é possível porque há entidades relacionadas");
 		}
 	}
 	
@@ -61,4 +62,10 @@ public class CategoriaService {
     	return new Categoria(objDto.getId(), objDto.getNome());
     }
 	
+    private void updateData (Categoria newObj, Categoria obj) {
+    	newObj.setNome(obj.getNome());
+    }
+    
+
+
 }
--- src/main/java/com/nomedaempresa/cursomc/services/ClienteService.java
+++ src/main/java/com/nomedaempresa/cursomc/services/ClienteService.java
@@ -1,19 +1,27 @@
 package com.nomedaempresa.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
-import com.nomedaempresa.cursomc.domain.Categoria;
 import com.nomedaempresa.cursomc.domain.Cliente;
+import com.nomedaempresa.cursomc.domain.enums.TipoCliente;
+import com.nomedaempresa.cursomc.domain.Cliente;
+import com.nomedaempresa.cursomc.dto.ClienteDTO;
 import com.nomedaempresa.cursomc.repositories.ClienteRepository;
+import com.nomedaempresa.cursomc.services.exception.DataIntegrityException;
 import com.nomedaempresa.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class ClienteService {
 	
-	@Autowired /* instancia a classe CategoriaRepository aqui */
+	@Autowired /* instancia a classe ClienteRepository aqui */
 	private ClienteRepository clienteRepository;
 	
 	public Cliente find(Integer id) {
@@ -22,4 +30,38 @@ public class ClienteService {
 		"Objeto nao encontrado Id: " + id + " Tipo: " + Cliente.class.getName()));
 		}
 	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return clienteRepository.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma cliente que possui produtos");
+		}
+	}
+	
+    public List<Cliente> findAll() {
+    	return clienteRepository.findAll();
+    }
+    
+    public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+    	PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+    	return clienteRepository.findAll(pageRequest);
+    }
+    
+    public Cliente fromDTO(ClienteDTO objDto) {
+    	return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+    }
+
+    private void updateData (Cliente newObj, Cliente obj) {
+    	newObj.setNome(obj.getNome());
+    	newObj.setEmail(obj.getEmail());
+    }
+    
 }
